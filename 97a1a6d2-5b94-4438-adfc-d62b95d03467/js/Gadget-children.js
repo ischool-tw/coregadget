1450995f3d8552b494e121d5d60fd4451e096745
bubu <yaqxyaqx@gmail.com>
2012-08-09 09:22:00 +0800

    加入家長小工具(一般) - 畫面套用 Twitter Bootstrap

--- 97a1a6d2-5b94-4438-adfc-d62b95d03467/js/Gadget-children.js
+++ 97a1a6d2-5b94-4438-adfc-d62b95d03467/js/Gadget-children.js
@@ -1,76 +1,93 @@
 ﻿/// <reference path="../include/jquery-1.7.2.min.js"/>
-var public_connection;
-var parent_connection = gadget.getContract("auth.parent");
-var myparent = {};
+var myparent = myparent || {};
+
+/// <reference path="../include/jquery-1.7.2.min.js"/>
+$(document).ready(function () {
+    $('#ParentCode').focus();
+
+    myparent.DrawingChildrenList = function () {
+        myparent.childrenList('.childrenList');
+    };
+
+    myparent.DrawingChildrenList();
+
+    $('#send').bind('click', function () {
+        $("div .control-group").removeClass("error");
+        $(".help-inline").html("");
+        myparent.setParentRelationship($('#ParentCode').val(), $('#IDNumber').val(), $('#Relationship').val());
+    });
+
+});
 
 myparent.setParentRelationship = function (parentCode, idNumber, relationship) {
-	public_connection = public_connection || gadget.getContract("parentguest");
-	public_connection.send({
-		service: 'Parent.SetParentRelationship',
-		body: {
-			Request: {
-				ParentCode: parentCode,
-				IDNumber: idNumber,
-				Relationship: relationship
-			}
-		},
-		result: function (response, error, http) {
-			if (error) {
-				switch (error.dsaError.header.DSFault.Fault.Code) {
-					case '001':
-						$('#ParentCode, #IDNumber').addClass('inputerror');
-						break;
-					case '002':
-						$('#ParentCode').addClass('inputerror');
-						break;
-				}
-				alert(error.dsaError.message);
-			} else {
-				if (myparent.parentPosition == true) {
-					DrawingChildrenList();					
-					alert('設定完成!');
-				} else {
-					alert('設定完成!請重新登入本系統，以獲得完整的功能。');
-					$('.message').html('關係設定完成，請重新登入本系統，以獲得完整的功能，謝謝您。');
-					$('#send').hide();
-				}				
-			}
-		}
-	});
+    myparent.public_connection = gadget.getContract("parentguest");
+    myparent.public_connection.send({
+        service: 'Parent.SetParentRelationship',
+        body: {
+            Request: {
+                ParentCode: parentCode,
+                IDNumber: idNumber,
+                Relationship: relationship
+            }
+        },
+        result: function (response, error, http) {
+            if (error) {
+                switch (error.dsaError.header.DSFault.Fault.Code) {
+                    case '001': //家長代碼或身分證號不正確
+                        $('#ParentCode, #IDNumber')
+                            .parent().parent().addClass("error")
+                            .find(".help-inline").html(error.dsaError.message);
+                        $('#ParentCode').focus();
+                        break;
+                    case '002': //此代碼已經設定過了
+                        $('#ParentCode')
+                            .focus()
+                            .parent().parent().addClass("error")
+                            .find(".help-inline").html(error.dsaError.message);
+                        break;
+                    case '003': //與家長建立關聯時發生錯誤
+                        $('#errorMessage').html('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><strong>設定失敗！</strong> 與家長建立關聯時發生錯誤</div>');
+                        break;
+                    default:
+                        $('#errorMessage').html('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><strong>設定失敗！</strong> ' + error.dsaError.message + '</div>');
+                }
+            } else {
+                myparent.DrawingChildrenList();
+
+                $('#editModal').modal("show");
+                $('#editModal h3').html("設定成功")
+                $('#editModal .modal-body').html('設定完成，建議您重新登入本系統，以獲得完整的功能，謝謝您。');                
+            }
+        }
+    });
 };
 
 myparent.childrenList = function (e) {
-//	parent_connection.ready(function () {
-		parent_connection.send({
-			service: 'My.Children',
-			body: '',
-			result: function (response, error, http) {
-				if (error) {
-					console.log(error);
-				} else {
-					var tempHtmlA = '', tempHtmlB = '', tempHtml = '';
-					$(response.Children.Child).each(function (index, item) {
-						tempHtml += '<tr>';
-						tempHtml += '<td>' + item.Name + '</td>';
-						tempHtml += '<td>' + item.ClassName + '</td>';
-						tempHtml += '<td>' + item.SeatNo + '</td>';
-						tempHtml += '<td>' + item.Relationship + '</td>';
-						tempHtml += '</tr>';
-					});
+    myparent.parent_connection = gadget.getContract("auth.parent");
+    myparent.parent_connection.ready(function () {
+        myparent.parent_connection.send({
+            service: 'My.Children',
+            body: '',
+            result: function (response, error, http) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var tempHtmlA = '', tempHtmlB = '', tempHtml = '';
+                    $(response.Children.Child).each(function (index, item) {
+                        tempHtml += '<tr>';
+                        tempHtml += '<td>' + item.Name + '</td>';
+                        tempHtml += '<td>' + item.ClassName + '</td>';
+                        tempHtml += '<td>' + item.SeatNo + '</td>';
+                        tempHtml += '<td>' + item.Relationship + '</td>';
+                        tempHtml += '</tr>';
+                    });
+                    tempHtmlA = '<div class="well"><table class="table table-condensed table-striped"><thead><tr><th>姓名</th><th>班級</th><th>座號</th><th>關係</th></tr></thead>';
+                    tempHtmlA += '<tbody>';
+                    tempHtmlB = '</tbody></table></div>';
 
-					tempHtmlA = '<table class="linear"><thead><tr><th>姓名</th><th>班級</th><th>座號</th><th>關係</th></tr></thead>';
-					tempHtmlA += '<tbody>';
-					tempHtmlB = '</tbody></table>';
-
-					$(e).html(tempHtmlA + tempHtml + tempHtmlB);
-				}
-			}
-		});
-//	});
-
-};
-
-
-var DrawingChildrenList = function () {
-	myparent.childrenList('.childrenList');
+                    $(e).html(tempHtmlA + tempHtml + tempHtmlB);
+                }
+            }
+        });
+    });
 };
