f70239dd8d5ae169d7a1e7a77854c76d666c53ae
jungfengpaulwang <paul.wang@ischool.com.tw>
2014-06-19 21:46:51 +0800

    Update emba.teacher.js

--- 4628e1c9-564e-46a0-b096-ef4eae022e48/js/emba.teacher.js
+++ 4628e1c9-564e-46a0-b096-ef4eae022e48/js/emba.teacher.js
@@ -1228,9 +1228,11 @@ var CreateEvent = function() {
             //  課程為「核心」課程，則顯示成績分佈統計表，反之隱藏。
             if (pSelectedCourse) {
                 if (pSelectedCourse.CourseType.indexOf("核心") >= 0) {
-                    $("#statistics-container").show();
-                } else {
-                    $("#statistics-container").hide();
+                    if (currentCourse.InputRule !== 1) {
+                        $("#statistics-container").show();
+                    } else {
+                        $("#statistics-container").hide();
+                    }
                 }
             } else {
                 $("#statistics-container").hide();
