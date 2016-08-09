1cbe4df7d1f83406a7b9375a25744933b5adfdea
Kevin Huang <kevin.huang@ischool.com.tw>
2016-08-09 20:23:31 +0800

    加上「查看本期評鑑公告」：在「問卷填答統計」裡，根據使用者身份，及兩學期前的評鑑問卷填答率，決定是否可讓使用者查詢課程評鑑值，作為選課參考。(邏輯：如果為新生，則直接查看。如果不是，則兩學期前的填答率需超過 70%，根據組態值決定)。

--- ae4ff68f-b2dc-47b7-9167-f448e7247b4a/js/surveyHistory.js
+++ ae4ff68f-b2dc-47b7-9167-f448e7247b4a/js/surveyHistory.js
@@ -1,7 +1,7 @@
 
 var connection = gadget.getContract("emba.survey.student");
 var oAchievingRate = {};
-var oConfiguration = {};
+var oConfiguration = {}; 
 var oSurveyHistoryTemplateDescriptionSubfix = 'survey-history-description';
 
 var CallbackQueue_History = {
@@ -102,6 +102,130 @@ var GetTextTemplate = function() {
         }
     });
 };
+
+var GetCanSeeCurriculumEvaluation = function() {
+    $('#show_course_evaluation').hide();
+    connection.send({
+        service: "ext.CanSeeEvaluationRate",
+        body: "<Request></Request>",
+        result: function (response, error, http){
+            if (error !== null) {
+                $("#mainMsg").html("<div class='alert alert-error'>\n  <button class='close' data-dismiss='alert'>×</button>\n  <strong>呼叫服務失敗，請稍候重試!</strong>(CanSeeEvaluationRate)\n</div>");
+            } else {
+                /*
+                    <myEntranceSchoolYear>104</myEntranceSchoolYear>
+                    <csSchoolYear>105</currentSchoolYear>
+                    <csSemester>0</currentSemester>
+                    <isNewStudent>false</isNewStudent>
+                    <targetSchoolYear>104</targetSchoolYear>
+                    <targetSemester>2</targetSemester>
+                    <surveyCount>8</surveyCount>
+                    <replyCount>8</replyCount>
+                    <answerRate>100</answerRate>
+                    <ansRateStandard>70</ansRateStandard>
+                    <canSee>true</canSee>
+                */
+                console.log(response);
+                if (response.canSee && response.canSee == "true") {
+                    var msg =  [ 
+                                "<div style='color:red;'>",
+                                response.csSchoolYear,  "學年度 第" , 
+                                response.csSemester , "學期 選課參考依據 : ",
+                                "<a href='#' class='alert-link' ",
+                                "   onclick='showEvaluation(", response.csSchoolYear , "," , response.csSemester ,");'>查看本期評鑑公告</a>"
+                            ].join("");
+                    if (response.isNewStudent && response.isNewStudent == "true") {
+                        msg =  [ 
+                                msg,
+                                "( 您是新生 )" ,
+                                "</div>"
+                            ].join("");
+                    }
+                    else {
+                        msg =  [ 
+                                msg,
+                                "( 您的 ", response.targetSchoolYear , "/" , 
+                                response.targetSemester , " 評鑑填答率為 ", 
+                                response.answerRate , "% , 符合查看條件。)" ,
+                                "</div>"
+                            ].join("");
+                    }
+                    msg += ["<div>查看參考依據，是依前2學期之問卷填答率達到：",
+                             response.ansRateStandard ,
+                              "% 才可以查看選課課程之評鑑值</div>"].join("");
+                    $('#show_course_evaluation').html(msg).show();
+                }
+                else {
+                    var msg =  [ 
+                                "<div style='color:red;'>",
+                                "( 您的 ", response.targetSchoolYear , "/" , 
+                                response.targetSemester , " 評鑑填答率為 ", 
+                                response.answerRate , "% , 不符合查看條件。)" ,
+                                "</div>"
+                            ].join("");
+                    $('#show_course_evaluation').html(msg).show();
+                }
+            }
+
+            CallbackQueue_History.JobFinished();
+        }
+    });
+};
+
+var showEvaluation = function(schoolyear, semester) {
+    var content = ["<Body><Request><SchoolYear>", schoolyear , "</SchoolYear><Semester>" , semester , "</Semester></Request></Body>"].join("");
+    console.log(content);
+    connection.send({
+        service: "ext.GetCurriculumEvaluation",
+        body: content ,
+        result: function (response, error, http){
+            if (error !== null) {
+                $("#mainMsg").html("<div class='alert alert-error'>\n  <button class='close' data-dismiss='alert'>×</button>\n  <strong>呼叫服務失敗，請稍候重試!</strong>(GetReplyHistory)\n</div>");
+            } else {
+                var content = parseEvaluation(response);
+                var html = ["<!doctype html>",
+                            "<head>",
+                            "   <meta charset='utf-8'>",
+                            "   <title>顯示課程評鑑值</title>",
+                            "</head>",
+                            "<body>",
+                            content ,
+                            "</body>",
+                            "</html>"
+                ].join("");
+
+
+            }
+
+            var myWindow = window.open("show_curriculum_evaluation.html", "MsgWindow", "width=600,height=400,location=no,toolbar=no,menubar=no,scrollbars=yes,resizable=yes,top=100,left=100");
+            // myWindow.document.write(html);
+            $(myWindow.document).ready(function() {
+                myWindow.data_content = html;
+                // myWindow.showContent(html);
+            });
+        }
+    });
+
+    var parseEvaluation = function(response) {
+        console.log(response);
+        var content= "<table class='table'>";
+        content += "<thead><tr><th>課程</th><th>課程代碼</th><th>評鑑值</th></thead><tbody>";
+        if (response.Response && response.Response.Evaluation) {
+            $(response.Response.Evaluation).each(function(index, item) {
+                content += "<tr>";
+                content += ["<td>" , item.SubjectName , "</td>"].join("");
+                content += ["<td>" , item.NewSubjectCode , "</td>"].join("");
+                content += ["<td>" , item.Value , "</td>"].join("");
+                content += "</tr>";
+            });
+        }
+        content += "</tbody></table>";
+
+        return content;
+    };
+
+};
+
 
 var GetReplyHistory = function() {
     connection.send({
@@ -215,6 +339,7 @@ $(document).ready(function() {
     CallbackQueue_History.Queue(ClearHTML);
     CallbackQueue_History.Queue(GetTextTemplate);
     CallbackQueue_History.Queue(GetAchievingRate);
+    CallbackQueue_History.Queue(GetCanSeeCurriculumEvaluation);
     CallbackQueue_History.Queue(GetReplyHistory);
     CallbackQueue_History.DeQueue(0);
 });
