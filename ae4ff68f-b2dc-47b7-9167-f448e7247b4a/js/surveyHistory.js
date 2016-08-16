e35fdb0b524c6ef247615789d55f729498b8f800
dylan.huang <dylan.huang@ischool.com.tw>
2016-08-16 17:15:14 +0800

    依據芝儀需求,修改文字呈現

--- ae4ff68f-b2dc-47b7-9167-f448e7247b4a/js/surveyHistory.js
+++ ae4ff68f-b2dc-47b7-9167-f448e7247b4a/js/surveyHistory.js
@@ -130,29 +130,29 @@ var GetCanSeeCurriculumEvaluation = function() {
                     var msg =  [ 
                                 "<div style='color:red;'>",
                                 response.csSchoolYear,  "學年度 第" , 
-                                response.csSemester , "學期 選課參考依據 : ",
+                                response.csSemester , "學期 ",
                                 "<a href='#' class='alert-link' ",
-                                "   onclick='showEvaluation(", response.csSchoolYear , "," , response.csSemester ,");'>查看本期評鑑公告</a>"
+                                "   onclick='showEvaluation(", response.csSchoolYear , "," , response.csSemester ,");'>課程評鑑值</a>"
                             ].join("");
                     if (response.isNewStudent && response.isNewStudent == "true") {
                         msg =  [ 
                                 msg,
-                                "( 您是新生 )" ,
+                                " (您是新生。)" ,
                                 "</div>"
                             ].join("");
                     }
                     else {
                         msg =  [ 
                                 msg,
-                                "( 您的 ", response.targetSchoolYear , "/" , 
-                                response.targetSemester , " 評鑑填答率為 ", 
-                                response.answerRate , "% , 符合查看條件。)" ,
+                                "<div>(您的 ", response.targetSchoolYear , "學年度 第" , 
+                                response.targetSemester , "學期 評鑑填答率為 ", 
+                                response.answerRate , "%  可查看本期評鑑值公告。)" ,
                                 "</div>"
                             ].join("");
                     }
-                    msg += ["<div>查看參考依據，是依前2學期之問卷填答率達到：",
-                             response.ansRateStandard ,
-                              "% 才可以查看選課課程之評鑑值</div>"].join("");
+                    //msg += ["<div>查看參考依據，是依前2學期之問卷填答率達到：",
+                             //response.ansRateStandard ,
+                              //"% 才可以查看選課課程之評鑑值</div>"].join("");
                     $('#show_course_evaluation').html(msg).show();
                 }
                 else {
