77871a7342420a94be481856462150ab0bbbe43d
ChunTai Chen <chuntaichen@ChunTaide-MacBook-Pro.local>
2018-06-06 12:07:51 +0800

    功能重構。

--- B62BABC4-CDE8-465F-BC7A-C0DE2ACB91EE/src/app/select_course/add-task-away.component.ts
+++ B62BABC4-CDE8-465F-BC7A-C0DE2ACB91EE/src/app/select_course/add-task-away.component.ts
@@ -16,17 +16,11 @@ import { AddDialogComponent } from './add-dialog.component';
   styles: []
 })
 export class AddTaskAwayComponent implements OnInit {
-  head: string;
-  accessPoint: string;
-  // 課程資訊
-  courseInfo: any;
+
   loading: boolean = true;
-  error: any;
-  courseInfoList: any;
-  courseList: any;
-  selCourseType: string = "";
-  selCourseInfo: courseInfo;
-  selCourseInfoServ: any;
+  saving: boolean = false;
+  subjectType: string;
+  currentStatus: any;
 
   constructor(private route: ActivatedRoute, private gadget: GadgetService, private selectCourseData: SelectCourseDataService, private router: Router, private dialog: MatDialog) { }
   // 取得 contract 連線。
@@ -34,66 +28,18 @@ export class AddTaskAwayComponent implements OnInit {
 
   async ngOnInit() {
     this.contract = await this.gadget.getContract('ischool.course_selection');
-    this.selCourseInfo = new courseInfo();
+    this.subjectType = this.route.snapshot.paramMap.get("subjectType");
     this.getData();
   }
 
-  // 取得可選清單
-  async getCanSelectCourseList() {
-
-    this.loading = true;
-    this.selCourseType = this.route.snapshot.paramMap.get("courseType");
-    let courseType = this.selCourseType;
-
+  async getData() {
     try {
-      // 呼叫 service。
-      const rsp1 = await this.contract.send('GetTakeAway', {
-        Request: {
-          Type: courseType
-        }
-      });
-      this.courseInfo = Utils.array(rsp1, "科目清單/科目");
-      // <科目系統編號>148273</科目系統編號>
-      // <科目名稱>歌聲傳奇</科目名稱>
-      // <已選人數>34</已選人數>
-      // <人數上限>60</人數上限>
+      this.loading = true;
+      this.currentStatus = await this.contract.send('GetSubjectList', { SubjectType: this.subjectType });
 
-      this.courseList = [];
-
-      for (const data of this.courseInfo) {
-        let co: subjectInfo = new subjectInfo();
-        co.subjectID = data["科目系統編號"];
-        co.subjectName = data["科目名稱"];
-        co.selectedCount = data["已選人數"];
-        co.maxCount = data["人數上限"];
-        this.courseList.push(co);
-      }
-
-      // 處理已選
-      let selCourse = Utils.array(rsp1, "科目清單/選上科目");
-      if (selCourse.length > 0) {
-        let course: courseInfo = new courseInfo();
-        course.typeName = this.selCourseType;
-        course.selectedCourse = selCourse[0]["科目名稱"];
-        course.isNoCourse = false;
-
-        let subj = new subjectInfo();
-        subj.subjectID = selCourse[0]["科目系統編號"];
-        subj.subjectName = selCourse[0]["科目名稱"];
-        course.subjectList = [subj];
-        this.selCourseInfo = course;
-
-        // 處理可選課程內已選
-        for (const ss of this.courseList) {
-          if (ss.subjectID === subj.subjectID) {
-            ss.isSelected = true;
-          } else {
-            ss.isSelected = false;
-          }
-        }
-      }
-
-      // console.log(this.courseList);
+      // this.currentStatus.Attend = [].concat(this.currentStatus.Attend || []);
+      this.currentStatus.Subject = [].concat(this.currentStatus.Subject || []);
+      console.log(this.currentStatus.Subject);
     } catch (err) {
       console.log(err);
     } finally {
@@ -101,73 +47,159 @@ export class AddTaskAwayComponent implements OnInit {
     }
   }
 
-  getData() {
-    this.getCanSelectCourseList();
-  }
-
-  showDialog(subject) {
-    // console.log(subject.subjectID);
-    const dig = this.dialog.open(AddDialogComponent, {
-      data: { subjectID: subject.subjectID, mode: '先搶先贏' }
-    });
-
-    dig.afterClosed().subscribe((v) => {
-      // alert(JSON.stringify(v));
-
-      this.sendData(v.subjectID);
-    });
-  }
-
-  // 直接加入選課
   joinCourse(subject) {
-    // console.log(subject);
-    this.sendData(subject.subjectID);
+    if (this.saving)
+      return;
+
+      this.setData(subject);
+    
   }
 
-  async sendData(subjectID) {
+  async setData(subject) {
     try {
-      // 呼叫 service。
-      // _.SetTakeAway
-      // <Request>
-      //   <SubjectID>148276</SubjectID>
-      //   <Type>'多元選修'</Type>
-      // </Request>
+      this.saving = true;
+      
+      await this.contract.send('SetTakeAway', { SubjectType: this.subjectType, SubjectID:subject.SubjectID });
 
-      let typeName = this.selCourseType;
-      const rsp2 = await this.contract.send('SetTakeAway', {
-
-        SubjectID: subjectID,
-        Type: typeName
-
-      });
-      let status = Utils.array(rsp2, "");
-      // 解析回傳訊息
-      if (status.length > 0) {
-        if (status[0].message !== "") {
-          alert(status[0].message);
-        } else {
-          // alert("已選完成");
-        }
-
-        // console.log(status[0].status);
-        // console.log(status[0].message);
-      }
-
-      // this.selectCourseData.selectCourseInfo = this.courseList.filter(x => x.subjectID === v.subjectID);
-      // console.log(this.selectCourseData.selectCourseInfo);
-      // console.log(status);
-      // this.router.navigate(['../../main'], {
-      //   relativeTo: this.route
-      // });
-      // console.log(this.courseList);
-
-      // 重整資料      
-      this.getCanSelectCourseList();
     } catch (err) {
-      console.log(err);
+      alert("SetTakeAway error:\n"+JSON.stringify(err));
     } finally {
-      this.loading = false;
+      // var self = this;
+      this.saving = false;
+     
+       this.getData();
     }
   }
+  // // 取得可選清單
+  // async getCanSelectCourseList() {
+
+  //   this.loading = true;
+  //   this.selCourseType = this.route.snapshot.paramMap.get("courseType");
+  //   let courseType = this.selCourseType;
+
+  //   try {
+  //     // 呼叫 service。
+  //     const rsp1 = await this.contract.send('GetTakeAway', {
+  //       Request: {
+  //         Type: courseType
+  //       }
+  //     });
+  //     this.courseInfo = Utils.array(rsp1, "科目清單/科目");
+  //     // <科目系統編號>148273</科目系統編號>
+  //     // <科目名稱>歌聲傳奇</科目名稱>
+  //     // <已選人數>34</已選人數>
+  //     // <人數上限>60</人數上限>
+
+  //     this.courseList = [];
+
+  //     for (const data of this.courseInfo) {
+  //       let co: subjectInfo = new subjectInfo();
+  //       co.subjectID = data["科目系統編號"];
+  //       co.subjectName = data["科目名稱"];
+  //       co.selectedCount = data["已選人數"];
+  //       co.maxCount = data["人數上限"];
+  //       this.courseList.push(co);
+  //     }
+
+  //     // 處理已選
+  //     let selCourse = Utils.array(rsp1, "科目清單/選上科目");
+  //     if (selCourse.length > 0) {
+  //       let course: courseInfo = new courseInfo();
+  //       course.typeName = this.selCourseType;
+  //       course.selectedCourse = selCourse[0]["科目名稱"];
+  //       course.isNoCourse = false;
+
+  //       let subj = new subjectInfo();
+  //       subj.subjectID = selCourse[0]["科目系統編號"];
+  //       subj.subjectName = selCourse[0]["科目名稱"];
+  //       course.subjectList = [subj];
+  //       this.selCourseInfo = course;
+
+  //       // 處理可選課程內已選
+  //       for (const ss of this.courseList) {
+  //         if (ss.subjectID === subj.subjectID) {
+  //           ss.isSelected = true;
+  //         } else {
+  //           ss.isSelected = false;
+  //         }
+  //       }
+  //     }
+
+  //     // console.log(this.courseList);
+  //   } catch (err) {
+  //     console.log(err);
+  //   } finally {
+  //     this.loading = false;
+  //   }
+  // }
+
+  // getData() {
+  //   this.getCanSelectCourseList();
+  // }
+
+  // showDialog(subject) {
+  //   // console.log(subject.subjectID);
+  //   const dig = this.dialog.open(AddDialogComponent, {
+  //     data: { subjectID: subject.subjectID, mode: '先搶先贏' }
+  //   });
+
+  //   dig.afterClosed().subscribe((v) => {
+  //     // alert(JSON.stringify(v));
+
+  //     this.sendData(v.subjectID);
+  //   });
+  // }
+
+  // // 直接加入選課
+  // joinCourse(subject) {
+  //   // console.log(subject);
+  //   this.sendData(subject.subjectID);
+  // }
+
+  // async sendData(subjectID) {
+  //   try {
+  //     // 呼叫 service。
+  //     // _.SetTakeAway
+  //     // <Request>
+  //     //   <SubjectID>148276</SubjectID>
+  //     //   <Type>'多元選修'</Type>
+  //     // </Request>
+
+  //     let typeName = this.selCourseType;
+  //     const rsp2 = await this.contract.send('SetTakeAway', {
+
+  //       SubjectID: subjectID,
+  //       Type: typeName
+
+  //     });
+  //     let status = Utils.array(rsp2, "");
+  //     // 解析回傳訊息
+  //     if (status.length > 0) {
+  //       if (status[0].message !== "") {
+  //         alert(status[0].message);
+  //       } else {
+  //         // alert("已選完成");
+  //       }
+
+  //       // console.log(status[0].status);
+  //       // console.log(status[0].message);
+  //     }
+
+  //     // this.selectCourseData.selectCourseInfo = this.courseList.filter(x => x.subjectID === v.subjectID);
+  //     // console.log(this.selectCourseData.selectCourseInfo);
+  //     // console.log(status);
+  //     // this.router.navigate(['../../main'], {
+  //     //   relativeTo: this.route
+  //     // });
+  //     // console.log(this.courseList);
+
+  //     // 重整資料      
+  //     this.getCanSelectCourseList();
+  //   } catch (err) {
+  //     console.log(err);
+  //   } finally {
+  //     this.loading = false;
+  //   }
+  // }
 
 }
